772bc025f1a8b95e8cd1dfe59459fd522f6ecc75
Akim <akimduvanov@gmail.com>
2025-05-30 19:38:22 +0200

    js9 smth

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,8 +1,8 @@
 
 // Описаний в документації
-//import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
-//import "simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
       {
